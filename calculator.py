e6102f0ee7ebf5bd3a7c63f592602d0f8886a6cb
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 16:37:59 -0700

    Revised styling

--- calculator.py
+++ calculator.py
@@ -17,26 +17,37 @@ repeat forever:
 from arithmetic import *
 
 while True:
-    read = input()
+    read = input('User input: ')
+    
     token = read.split(" ")
+    
     if token[0] == '+':
         print(add(float(token[1]), float(token[2])))
+    
     elif token[0] == '-':
         print(subtract(float(token[1]), float(token[2])))
+    
     elif token[0] == '*':
         print(multiply(float(token[1]), float(token[2])))
+    
     elif token[0] == '/':
         print(divide(float(token[1]), float(token[2])))
+    
     elif token[0] == 'square':
         print(square(float(token[1])))
+    
     elif token[0] == 'cube':
         print(cube(float(token[1])))
+    
     elif token[0] == 'pow':
         print(power(float(token[1]), float(token[2])))
+    
     elif token[0] == 'mod':
         print(mod(float(token[1]), float(token[2])))
+    
     elif token[0] == 'q':
         break
+    
     else:
         print('Invalid. Try again.')
 
