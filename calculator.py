167f0b6c598d7aed97457713a34ae5773d4f3230
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 16:26:07 -0700

    Finished

--- calculator.py
+++ calculator.py
@@ -19,7 +19,24 @@ from arithmetic import *
 while True:
     read = input()
     token = read.split(" ")
-    if token[0] == 'q':
-        quit
-    elif token[0] == '+':
+    if token[0] == '+':
         print(add(float(token[1]), float(token[2])))
+    elif token[0] == '-':
+        print(subtract(float(token[1]), float(token[2])))
+    elif token[0] == '*':
+        print(multiply(float(token[1]), float(token[2])))
+    elif token[0] == '/':
+        print(divide(float(token[1]), float(token[2])))
+    elif token[0] == 'square':
+        print(square(float(token[1])))
+    elif token[0] == 'cube':
+        print(cube(float(token[1])))
+    elif token[0] == 'pow':
+        print(power(float(token[1]), float(token[2])))
+    elif token[0] == 'mod':
+        print(mod(float(token[1]), float(token[2])))
+    elif token[0] == 'q':
+        break
+    else:
+        print('Invalid. Try again.')
+
